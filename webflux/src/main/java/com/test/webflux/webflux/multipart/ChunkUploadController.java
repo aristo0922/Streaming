0baa511a918a3df39c0c2daa3cc86fd2455eb27c
aristo <dkfud2121@naver.com>
2024-05-23 15:17:34 +0900

    error ? 업로드는 되는데(아마) 분할한 chunk index를 못가져온다.

--- webflux/src/main/java/com/test/webflux/webflux/multipart/ChunkUploadController.java
+++ webflux/src/main/java/com/test/webflux/webflux/multipart/ChunkUploadController.java
@@ -1,2 +1,36 @@
-package com.test.webflux.webflux.multipart;public class ChunkUploadController {
+package com.test.webflux.webflux.multipart;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class ChunkUploadController {
+    private final ChunkUploadService chunkUploadService;
+
+    @GetMapping("/chunk")
+    public String chunkUploadPage(){
+        return "chunk";
+    }
+
+    @ResponseBody
+    @PostMapping("/chunk/upload")
+    public ResponseEntity<String> chunkUpload(@RequestParam("chunk") MultipartFile file,
+                                              @RequestParam("chunkNumber") int chunkNumber,
+                                              @RequestParam("totalChunks") int totalChunks) throws IOException {
+        boolean isDone = chunkUploadService.chunkUpload(file, chunkNumber, totalChunks);
+        return isDone? ResponseEntity.ok("FileUploaded successfully"):
+                ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).build();
+    }
+    @ResponseBody
+    @GetMapping("/chunk/upload/{key}")
+    public ResponseEntity<?> getLastChunkNumber(@PathVariable String key) throws IOException {
+        return ResponseEntity.ok(chunkUploadService.getLastChunkNumber(key));
+    }
 }
--- webflux/src/main/java/com/test/webflux/webflux/multipart/ChunkUploadService.java
+++ webflux/src/main/java/com/test/webflux/webflux/multipart/ChunkUploadService.java
@@ -1,2 +1,46 @@
-package com.test.webflux.webflux.multipart;public class ChunkUploadService {
+package com.test.webflux.webflux.multipart;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.UUID;
+
+@Service
+public class ChunkUploadService {
+    public boolean chunkUpload(MultipartFile file, int chunkNumber, int totalChunks) throws IOException{
+        String uploadDir = "video";
+        File dir = new File(uploadDir);
+        if(!dir.exists()) dir.mkdirs();
+
+        String filename = file.getOriginalFilename() + ".part" + chunkNumber;
+        Path filePath = Paths.get(uploadDir, filename);
+        Files.write(filePath, file.getBytes());
+
+        if(chunkNumber == totalChunks -1){
+            String [] split = file.getOriginalFilename().split("\\.");
+            String outputFilename = UUID.randomUUID()+"."+split[split.length-1];
+            Path outputFile = Paths.get(uploadDir, outputFilename);
+            Files.createFile(outputFile);
+
+            for(int i=0;i<totalChunks;i++){
+                Path chunkFile = Paths.get(uploadDir, file.getOriginalFilename()+".part"+i);
+                Files.write(outputFile, Files.readAllBytes(chunkFile), StandardOpenOption.APPEND);
+                Files.delete(chunkFile);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public int getLastChunkNumber(String key) {
+        Path temp = Paths.get("video", key);
+        String[] list = temp.toFile().list();
+        return list == null? 0:Math.max(list.length-2,0);
+    }
 }
